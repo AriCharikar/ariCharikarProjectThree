cc39e218d3be61da8b90518e9a374a307303d5fd
Ari Charikar <ari.charikar1@gmail.com>
2020-07-27 05:50:16 -0400

    Reworked javascript and jquery, restructured html :fire:

--- script.js
+++ script.js
@@ -2,46 +2,27 @@ let score = 0; //Creating int container for score
 const totalQuestions = 4;
 let point = 1;
 
-function init() {
-    sessionStorage.setItem('a1', 'b');
-    sessionStorage.setItem('a2', 'c');
-    sessionStorage.setItem('a3', 'a');
-    sessionStorage.setItem('a4', 'd');
-}
-
 $(document).ready(function() {
     //Hide all questions before user begins game
-    $('.qContainer').hide();
+    $('.qContainer, .winMsg').hide();
 
-    //Show first question
-    $('#q1').show();
-
-    //Hide first and show second question
-    $('#q1 #subButtonOne').click(function(){
+    //Show questions!
+    $('.startBtn').click(function(){
         $('.qContainer').hide();
-        $('#q2').show();
+        $('.btn, .btnCorrect, .hide').show();
         return false;
     });
 
-    $('#q2 #subButtonTwo').click(function(){
-        $('.qContainer').hide();
-        $('#q3').show();
-        return false;
+    $('.btnCorrect').click(function(){
+        score = score + 1;
     });
 
-    $('#q3 #subButtonThree').click(function(){
-        $('.qContainer').hide();
-        $('#q4').show();
-        return false;
-    });
+    if (point === totalQuestions) {
+        $('.winMsg').show();
+    };
 
-    $('#q4 #subButtonFour').click(function(){
-        $('.qContainer').hide();
-        $('.results').show();
-        return false;
-    });
+
+
+    
 
 });
-
-// Add event listener
-window.addEventListener('load', init, false);
